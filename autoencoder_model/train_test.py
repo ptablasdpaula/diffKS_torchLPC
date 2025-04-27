98cea6e2ba51d3dd1f5c06ab6b83f8977cc0b727
Pablo Tablas <acw794@qmul.ac.uk>
2025-04-27 20:18:26 +0100

    Adds test.sh training and fixes != mps line

--- autoencoder_model/train_test.py
+++ autoencoder_model/train_test.py
@@ -98,7 +98,7 @@ def main():
                         batch_size=config["batch_size"],
                         shuffle=True,
                         drop_last=True,
-                        pin_memory=True if device.type is not "mps" else False,
+                        pin_memory=True if device.type != "mps" else False,
                         num_workers=config["num_workers"])
 
     print(f"[INFO] Memory after dataloader: {process.memory_info().rss / 1024 ** 3:.2f} GB")
