3732f6929a080d68e0507e2f94756353cdd40141
Pablo Tablas <acw794@qmul.ac.uk>
2025-05-04 18:16:13 +0100

    creates experiments dir

--- paths.py
+++ paths.py
@@ -2,10 +2,9 @@ from pathlib import Path
 
 ROOT_DIR   = Path(__file__).resolve().parent
 DATA_DIR   = ROOT_DIR / "data"
-NSYNTH_DIR = DATA_DIR  / "nsynth"
 
-# existing constants stay as‑is if you want quick aliases
-NSYNTH_TEST_DIR  = NSYNTH_DIR / "nsynth-test"
-NSYNTH_TRAIN_DIR = NSYNTH_DIR / "nsynth-train"
-NSYNTH_VALID_DIR = NSYNTH_DIR / "nsynth-valid"
+NSYNTH_DIR = DATA_DIR  / "nsynth"
 NSYNTH_PREPROCESSED_DIR = DATA_DIR / "nsynth-preprocessed"
+SYNTHETIC_DIR = DATA_DIR / "synthetic"
+
+AUTOENCODER_MODEL = ROOT_DIR / "experiments" / "autoencoder" / "best_model.pth"
